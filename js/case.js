8c79be9f908224f880cb2270c7c2b6c71ecf7388
sabbir1257 <masabbir1257@gmail.com>
2023-02-16 01:48:45 +0600

    push

--- js/case.js
+++ js/case.js
@@ -1,8 +1,43 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(){
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString); 
+/*
+1. add event listener to the case push button 
+2. get the value inside the case number field (input field)
+3. convert the number to an integer
+4. caleulate the new case number
+5. set the value to the case number field
+*/
 
-    const newCaseNumber = previousCaseNumber + 1
-    caseNumberField.value = newCaseNumber;
-})
+function updateCaseNumber(isIncrease){
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberString = caseNumberField.value;
+  const previousCaseNumber = parseInt(caseNumberString);
+
+  let newCaseNumber;
+
+  if(isIncrease === true){
+    newCaseNumber = previousCaseNumber + 1;
+  }
+  else{
+    newCaseNumber = previousCaseNumber - 1;
+  }
+
+  caseNumberField.value = newCaseNumber;
+
+  return newCaseNumber;
+
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  const newCaseNumber = updateCaseNumber(true);
+
+  const caseTotalPrice = newCaseNumber * 59;
+
+  const caseTotalElement = document.getElementById('case-total');
+
+  caseTotalElement.innerText = caseTotalPrice;
+});
+
+document.getElementById("btn-case-minus").addEventListener("click", function () {
+  updateCaseNumber(false);
+});
+
+
